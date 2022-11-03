ccb5e6586ad7d8ece8eb5b855b9f47c59c80e87e
kashifflavio <63181186+kashifflavio@users.noreply.github.com>
2022-11-02 20:44:22 -0400

    Update 1-Basic Example.ps1

--- 1-Basic Example.ps1
+++ 1-Basic Example.ps1
@@ -12,7 +12,7 @@ $Pipedrive_BaseURL="https://$Pipedrive_domain.pipedrive.com/v1/"
 # Get All Persons
 #https://developers.pipedrive.com/docs/api/v1/#!/Persons/getPersons
 #https://company.pipedrive.com/v1/persons?start=0&api_token=3e3c33c3a3d3333dcd333ab3c333333c33b33fc
-$Url=$Pipedrive_BaseURL+"persons?start=0&api_token=$PipeDriveAPI"
+$Url=$Pipedrive_BaseURL+"persons?start=0&api_token=$PipeDrive_APIKey"
 
 $Result=Invoke-RestMethod -uri $URL -Method GET 
 
